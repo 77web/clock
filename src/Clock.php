6d0cf169dc4c389595dde3c4cac7dedb221286a6
77web <h-hishida@quartetcom.co.jp>
2020-05-16 14:40:35 +0900

    Clockクラス

---
+++ src/Clock.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Nanaweb\Clock;
+
+class Clock
+{
+    /**
+     * @var string
+     */
+    private $timezone;
+
+    /**
+     * @param ?string $timezone
+     */
+    public function __construct(?string $timezone = null)
+    {
+        $this->timezone = $timezone ?? 'Asia/Tokyo';
+    }
+
+
+    public function today()
+    {
+        return $this->now()->setTime(0,0,0);
+    }
+
+    public function now()
+    {
+        return new \DateTimeImmutable('', new \DateTimeZone('Asia/Tokyo'));
+    }
+}
